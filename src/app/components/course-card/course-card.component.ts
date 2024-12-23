bfa67954d975d03c9a2121ad1278c5c86492a3c4
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-12-23 01:45:45 -0500

    ngSwitch at course-card component

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,13 +1,15 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Course} from '../../model/course';
-import {NgClass, NgIf, NgStyle} from '@angular/common';
+import {NgClass, NgIf, NgStyle, NgSwitch, NgSwitchCase} from '@angular/common';
 
 @Component({
   selector: 'course-card',
   imports: [
     NgIf,
     NgClass,
-    NgStyle
+    NgStyle,
+    NgSwitch,
+    NgSwitchCase
   ],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
