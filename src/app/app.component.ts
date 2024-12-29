41b150478079d248ad2bc3fb6c2160b52611dffe
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-12-29 06:26:57 -0500

    host decorator at DI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,46 @@ import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {CoursesService} from './services/courses.service';
 
-function courseServiceProvider(http: HttpClient): CoursesService {
-  return new CoursesService(http);
-}
+// function courseServiceProvider(http: HttpClient): CoursesService {
+//   return new CoursesService(http);
+// }
+//
+// const COURSES_SERVICE = new InjectionToken<CoursesService>('COURSES_SERVICE');
 
-const COURSES_SERVICE = new InjectionToken<CoursesService>('COURSES_SERVICE');
+// @Component({
+//     selector: 'app-root',
+//     templateUrl: './app.component.html',
+//     styleUrls: ['./app.component.css'],
+//     standalone: false,
+//   providers: [{
+//   provide: COURSES_SERVICE,
+//   useFactory: courseServiceProvider,
+//   deps: [HttpClient]
+// }]})
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+//   standalone: false,
+//   providers: [{
+//     provide: CoursesService,
+//     useClass: CoursesService
+//   }]})
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false,
-  providers: [{
-  provide: COURSES_SERVICE,
-  useFactory: courseServiceProvider,
-  deps: [HttpClient]
-}]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: false,
+  providers: [CoursesService]
 })
 export class AppComponent implements OnInit {
 
 
   courses$: Observable<Course[]>;
 
-  constructor(@Inject(COURSES_SERVICE) private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
